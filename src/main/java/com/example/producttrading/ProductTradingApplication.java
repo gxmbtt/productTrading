4366222feba1526e3174bf94dd60d43eb128a6dd
guoxin <2259929962@qq.com>
2026-04-02 16:09:13 +0800

    git init

---
+++ src/main/java/com/example/producttrading/ProductTradingApplication.java
@@ -0,0 +1,13 @@
+package com.example.producttrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductTradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductTradingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/producttrading/ProductTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.producttrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductTradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
